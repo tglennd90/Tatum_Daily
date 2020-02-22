b726516a9dec441fd4a47364604d6809315d246f
Tatum Daily <tgd9019@outlook.com>
2020-02-22 09:49:18 -0600

    Portfolio Skeleton Done/Starting Bio Skeleton

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,5 +16,32 @@
 // Functions //
 // ============================================= //
 
+    // Portfolio Slide-Show //
+    // ============================================= //
+    var slideIndex = 0;
+
+    function showSlides() {
+        
+        const slides = document.getElementsByClassName("portfolioSlide");
+
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+
+        slides[slideIndex-1].style.display = "block";
+
+        setTimeout(showSlides, 3500);
+    }
+
 // Calls //
 // ============================================= //
+
+showSlides();
+
+  
